95ae521746db0fa1aa104b61e1c7ecb4f8c31868
OatmealJester <drrr032@gmail.com>
2024-10-05 14:41:55 -0400

    button offical done

--- main/buttons.c
+++ main/buttons.c
@@ -4,16 +4,22 @@
 static const char *TAG = "BUTTON_APP";
 
 #define NUM_BUTTONS 3
+#define CONFIG_BUTTON_LONG_PRESS_TIME_MS 1000  // 1 seconds for long press
+#define CONFIG_BUTTON_SHORT_PRESS_TIME_MS 20  // 0.2 seconds for short press
 
 // GPIO pins for the buttons
-//23 are normal buttons
-//21 is petentiometer button
 const uint8_t button_pins[NUM_BUTTONS] = {23, 21};
 
 // Callback function for button presses
 void button_press_cb(void *btn_handle, void *usr_data) {
     uint8_t btn_idx = (uint8_t)(intptr_t)usr_data;
-    ESP_LOGI(TAG, "Button %d Pressed", btn_idx + 1);
+    ESP_LOGI(TAG, "Button %d Short Pressed", btn_idx + 1);
+}
+
+// Callback function for long button presses
+void button_long_press_cb(void *btn_handle, void *usr_data) {
+    uint8_t btn_idx = (uint8_t)(intptr_t)usr_data;
+    ESP_LOGI(TAG, "Button %d Long Pressed", btn_idx + 1);
 }
 
 void app_main() {
@@ -23,6 +29,8 @@ void app_main() {
         // Create button handle
         btn_handles[i] = iot_button_create(&(button_config_t){
             .type = BUTTON_TYPE_GPIO,
+            .long_press_time = CONFIG_BUTTON_LONG_PRESS_TIME_MS,
+            .short_press_time = CONFIG_BUTTON_SHORT_PRESS_TIME_MS,
             .gpio_button_config = {
                 .gpio_num = button_pins[i],
                 .active_level = 0,
@@ -34,9 +42,14 @@ void app_main() {
             continue; // Proceed to create the next button
         }
 
-        // Register the callback, passing the button index as user data
+        // Register the callback for short press
         if (iot_button_register_cb(btn_handles[i], BUTTON_SINGLE_CLICK, button_press_cb, (void *)(intptr_t)i) != ESP_OK) {
-            ESP_LOGE(TAG, "Failed to register callback for Button %d", i + 1);
+            ESP_LOGE(TAG, "Failed to register short press callback for Button %d", i + 1);
+        }
+
+        // Register the callback for long press
+        if (iot_button_register_cb(btn_handles[i], BUTTON_LONG_PRESS_START, button_long_press_cb, (void *)(intptr_t)i) != ESP_OK) {
+            ESP_LOGE(TAG, "Failed to register long press callback for Button %d", i + 1);
         }
     }
 }
